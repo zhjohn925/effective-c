619c48439ab3aac5300a2be7275e3de1162fb526
John Zhuang <zhjohn925@gmail.com>
2018-10-07 22:28:38 -0700

    update

--- function_parameters_has_own_copy.c
+++ function_parameters_has_own_copy.c
@@ -16,6 +16,14 @@ void takeArray(int *arr) {
    printf("takeArray(), &arr=%p, arr=%p &arr[0]=%p\n", &arr, arr, &arr[0]);
 }
 
+//Compiler creates return copy (copy of c)
+//Therefore, return copy of c,  not local variable c 
+//local variable c was collected by garbage collector.
+int add(int a, int b) {
+   int c = a + b;
+   return c;
+}
+
 //Output:
 // main() before: &p=0x7ffee85dbaa0, p=0x7ffee85dbaa8, *p=11, &num=0x7ffee85dbaa8
 // changePtr():   &p=0x7ffee85dba78, p=0x7ffee85dbaa8, *p=99
@@ -29,6 +37,9 @@ int main()
    changePtr(p);
    printf("main() after:  &p=%p, p=%p, *p=%d, &num=%p\n", &p, p, *p, &num);
    
+   // before takeArray(), &arr=0x7ffee6613aac, arr=0x7ffee6613aac
+   // takeArray(), &arr=0x7ffee6613a68, arr=0x7ffee6613aac &arr[0]=0x7ffee6613aac
+
    int arr[] = {1, 2, 3};
    printf("before takeArray(), &arr=%p, arr=%p\n", &arr, arr);
    takeArray(arr);
